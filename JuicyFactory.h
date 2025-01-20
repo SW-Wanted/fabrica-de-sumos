8b08195750ff6803028d8c8dc152c47e10879093
Emanuel dos Santos <53709387+SW-WANTED@users.noreply.github.com>
2025-01-21 00:04:08 +0100

    Atualiza .gitignore, remove arquivos desnecessários e ajusta funções na Fila e JuicyFactory; exclui arquivos da Pilha e Lista.

--- JuicyFactory.h
+++ JuicyFactory.h
@@ -1,20 +1,6 @@
 #include "Fila.h"
-<<<<<<< HEAD
 void inserirPacoteManual(Fila *filaPA, Fila *filaPB);
 void inserirPacoteAutomatico(Fila *filaPA, Fila *filaPB, const char *nomeArquivo);
 void validarFila(Fila *fila, char tipoEsperado);
 void processarFilaEnchimento(Fila *filaEnchimento, Fila *filaEmbalamento, int limiteGrupo);
 void imprimirRelatorios(Fila *filaPA, Fila *filaPB, int descartadosA, int descartadosB, int processadosA, int processadosB);
-=======
-
-// Menus e opcoes
-void inserirPacoteManual(Fila * filaPA, Fila * filaPB);
-void inserirPacoteAutomatico(Fila * filaPA, Fila * filaPB, const char * nomeArquivo);
-void validarFila(Fila * fila, char tipoEsperado);
-void processarFilaEnchimento(Fila * filaEnchimento, Fila * filaEmbalamento, int limiteGrupo);
-void imprimirRelatorios(Fila * filaPA, Fila * filaPB, int descartadosA, int descartadosB, int processadosA, int processadosB);
-
-// Fun��es auxiliares
-Pacote criarPacote(char tipo, float peso);
-int validarPacote(Pacote pacote, char tipoEsperado);
->>>>>>> a53313eae0fa86af99ed01928c152b82480ab18c
