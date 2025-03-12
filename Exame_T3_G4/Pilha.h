59094a1446f2e78066cb63ca9e244b0fce00c24f
Emanuel dos Santos <ernelemanuel10@hotmail.com>
2025-03-12 01:42:12 +0100

    Reajuste da estrutura do projeto

--- Exame_T3_G4/Pilha.h
+++
@@ -1,8 +0,0 @@
-#include "Fila.h"
-typedef struct pilha Pilha;
-
-Pilha *criarPilha(int limite);
-void empilhar(Pilha *pilha, Fila *fila);
-void desempilhar(Pilha *pilha);
-void imprimirPilha(Pilha *pilha);
-void destruirPilha(Pilha *pilha);
